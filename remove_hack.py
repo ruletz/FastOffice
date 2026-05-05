235c21b628f30a50a6744c3ad31cbf320672ca00
ruleta <ruleta.m@outlook.com>
2026-05-05 03:01:46 +0100

    v1.0 - Official High-Performance Release

--- remove_hack.py
+++
@@ -1,8 +0,0 @@
-import codecs, re
-html_path = 'c:/Projects/OnlyOffice/app/editors/web-apps/apps/documenteditor/main/index.html'
-with codecs.open(html_path, 'r', 'utf8') as f:
-    text = f.read()
-text = re.sub(r'<script id="canvas_hijack">.*?</script>\s*', '', text, flags=re.DOTALL)
-with codecs.open(html_path, 'w', 'utf8') as f:
-    f.write(text)
-print('Canvas hack removed')
